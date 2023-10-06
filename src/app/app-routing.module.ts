3306722f9bd044fb85812065a1ca654c41b634f3
Edis MR <edisanthony@gmail.com>
2023-10-06 16:56:27 -0600

    material design and components started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SongComponent } from './pages/song/song.component';
+import { UsersListComponent } from './pages/users-list/users-list.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'song/:id',component: SongComponent},
+  {path: 'users', component:UsersListComponent},
+  {path: 'profile', component:ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersListComponent } from './pages/users-list/users-list.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SongComponent } from './pages/song/song.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+const MaterialImports = [
+  MatToolbarModule
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersListComponent,
+    ProfileComponent,
+    SongComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialImports
   ],
   providers: [],
   bootstrap: [AppComponent]
