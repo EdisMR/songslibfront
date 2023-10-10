1776f231f9ae9dc6b877629541fdbb645e7adc0c
Edis MR <edisanthony@gmail.com>
2023-10-10 09:53:04 -0600

    home page functionalities works

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { categoriesListEnv } from 'src/app/environment/environment';
 import { categoriesInterface } from 'src/app/interfaces/categories.interface';
 import { songInterface } from 'src/app/interfaces/song.interface';
@@ -9,13 +11,84 @@ import { SongService } from 'src/app/services/song.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  constructor(private songSvc:SongService){
-    this.songSvc.songsList$.subscribe(data=>{
-      this.songsList=data
-    })
+export class HomeComponent implements OnDestroy {
+  constructor(
+    private _fb: FormBuilder,
+    private songSvc: SongService
+  ) {
+    this.buildForm()
+    this.filterData()
   }
 
-  public categories:categoriesInterface=categoriesListEnv
-  songsList:songInterface[]=[]
+  public songsListSource: songInterface[] = []
+  public songsListFiltered: songInterface[] = []
+  private songsSubscription: Subscription = this.songSvc.songsList$
+    .subscribe(songs => {
+      this.songsListSource = songs
+    })
+  public categories: categoriesInterface = categoriesListEnv
+
+  public get resultsFrom(): string {
+    let resultsFrom = ''
+    if (this.searchterm.length >= 4) {
+      resultsFrom = this.searchterm
+    }
+    if (this.choosenCategory.length > 0) {
+      resultsFrom = this.choosenCategory
+    }
+    return resultsFrom
+  }
+
+  chooseOneCategory(categorySelected: string, preventSearchProblem?: boolean) {
+    if (!preventSearchProblem) {
+      this.formSearch.reset()
+    }
+    this.choosenCategory = categorySelected
+    this.songsListFiltered = []
+    this.filterData()
+  }
+  choosenCategory = ''
+
+
+  filterData() {
+    let songsLength: number = this.songsListSource.length
+    this.songsListFiltered = []
+    for (let i = 0; i < songsLength; i++) {
+      if (this.choosenCategory.length > 0) {
+        if (this.songsListSource[i].categories.includes(this.choosenCategory)) {
+          this.songsListFiltered.push(this.songsListSource[i])
+        }
+      }
+      if (this.searchterm.length >= 4) {
+        if (((this.songsListSource[i].title).toLocaleLowerCase()).includes((this.searchterm).toLocaleLowerCase())) {
+          this.songsListFiltered.push(this.songsListSource[i])
+        }
+      }
+    }
+    if (this.searchterm.length < 4 && this.choosenCategory.length == 0) {
+      this.songsListFiltered = this.songsListSource
+    }
+  }
+
+  /* form */
+  public formSearch!: FormGroup
+  private formSearchSubscription!: Subscription
+  private buildForm() {
+    this.formSearch = this._fb.group({
+      searchterm: ['']
+    })
+    this.formSearchSubscription = this.formSearch.valueChanges
+      .subscribe(data => {
+        this.chooseOneCategory('', true)
+        this.filterData()
+      })
+  }
+  public get searchterm() {
+    return this.formSearch.get('searchterm')?.value || ''
+  }
+
+  ngOnDestroy(): void {
+    this.songsSubscription.unsubscribe()
+    this.formSearchSubscription.unsubscribe()
+  }
 }
--- src/app/services/song.service.ts
+++ src/app/services/song.service.ts
@@ -35,7 +35,7 @@ export class SongService {
     linkstring: "test",
     lyric: "Dm%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520A7%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Dm%250A%25C3%2593diame%2520por%2520piedad%252C%2520yo%2520te%2520lo%2520pido%250AD7%2Cquerido%250AF%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520A7%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Dm%250AQue%2520tan%2520solo%2520se%2520odia%252C%2520lo%2520querido",
     files: ["randomfile1.pdf", "randomfile2.mp3", "randomfile3.aac"],
-    categories: ["categoria1", "categoria2", "categoria3"],
+    categories: ["categoria1", "entrada", "categoria3"],
     active: true
   },{
     public_id: "5f6a5d1d-0a8a-4e2b-9d1d-6f9e0b2c0c1b",
@@ -47,7 +47,7 @@ export class SongService {
     linkstring: "test",
     lyric: "Dm%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520A7%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Dm%250A%25C3%2593diame%2520por%2520piedad%252C%2520yo%2520te%2520lo%2520pido%250AD7%2Cquerido%250AF%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520A7%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Dm%250AQue%2520tan%2520solo%2520se%2520odia%252C%2520lo%2520querido",
     files: ["randomfile1.pdf", "randomfile2.mp3", "randomfile3.aac"],
-    categories: ["categoria1", "categoria2", "categoria3"],
+    categories: ["categoria1", "envío (salida)", "categoria3"],
     active: true
   },{
     public_id: "5f6a5d1d-0a8a-4e2b-9d1d-6f9e0b2c0c1b",
@@ -59,7 +59,7 @@ export class SongService {
     linkstring: "test",
     lyric: "Dm%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520A7%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Dm%250A%25C3%2593diame%2520por%2520piedad%252C%2520yo%2520te%2520lo%2520pido%250AD7%2Cquerido%250AF%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520A7%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520Dm%250AQue%2520tan%2520solo%2520se%2520odia%252C%2520lo%2520querido",
     files: ["randomfile1.pdf", "randomfile2.mp3", "randomfile3.aac"],
-    categories: ["categoria1", "categoria2", "categoria3"],
+    categories: ["categoria1", "jesus", "categoria3"],
     active: true
   }]
   private songsList: BehaviorSubject<songInterface[]> = new BehaviorSubject<songInterface[]>(this.allSongs)
