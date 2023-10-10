17a7ebf173509b21628f507716de8c2ab914fff4
Edis MR <edisanthony@gmail.com>
2023-10-10 06:59:12 -0600

    style improvements

---
+++ src/app/interfaces/categories.interface.ts
@@ -0,0 +1,49 @@
+export interface categoriesInterface {
+	"misa": [
+		"entrada",
+		"ten piedad",
+		"gloria",
+		"aleluya",
+		"ofrendas",
+		"santo",
+		"cordero",
+		"comunion (eucaristia)",
+		"envío (salida)"
+	],
+	"tiempo liturgico": [
+		"adviento",
+		"navidad",
+		"epifania",
+		"tiempo ordinario",
+		"cuaresma",
+		"domingo de ramos",
+		"jueves santo",
+		"viernes santo",
+		"vigilia pascual",
+		"sabado santo",
+		"domingo santo",
+		"resurreccion de jesus",
+		"semana santa",
+		"pascua",
+		"cuarto domingo de pascua",
+		"setimo domingo de pascua",
+		"pentecostes",
+		"cristo rey"
+	],
+	"sacramentos": [
+		"bautismo",
+		"confirmación",
+		"eucaristia",
+		"penitencia",
+		"uncion de los enfermos",
+		"orden sacerdotal",
+		"matrimonio"
+	],
+	"santos": [
+		"santa maria madre de dios",
+		"maria",
+		"jesus",
+		"ssma trinidad",
+		"espiritu santo"
+	]
+}
